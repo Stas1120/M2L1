4a04f18a2061c20930aa3bc8feda577d543a134b
Stas1120 <stas_koshelev2024@mail.ru>
2025-03-28 23:53:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import telebot
+import os
+import random
+import requests
+images = os.listdir('images')
+
+
+bot = telebot.TeleBot('7663125275:AAEOh5TO84wldZCSM71hX2bs-ZvZO-I31l8')
+
+@bot.message_handler(commands=['meme'])
+def send_meme(massege):
+    image = random.choice(images)
+    with open(f'images/{image}', 'rb') as f:
+        bot.send_photo(massege.chat.id, f)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+    
+    
+@bot.message_handler(commands=['dog'])
+def dog(message):
+    '''По команде duck вызывает функцию get_duck_image_url и отправляет URL изображения утки'''
+    image_url = get_dog_image_url()
+    bot.reply_to(message, image_url)
+
+bot.polling()
